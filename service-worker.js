79ef26ac34160bda0acc9805ded2aa991d5bb131
Benjamin Steidl <localartpool@gmail.com>
2020-08-12 22:42:34 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a1e01a0c02e59f337bfeefc307df2460.js"
+  "/precache-manifest.1263b309612715bf23fc778234c7f3e7.js"
 );
 
 self.addEventListener('message', (event) => {
