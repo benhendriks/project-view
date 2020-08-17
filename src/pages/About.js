39e36c35e34ea4c80ff4ce923f8bd9648d7cc6f6
Benjamin Steidl <localartpool@gmail.com>
2020-08-17 09:37:49 +0200

    Worked on about text

--- src/pages/About.js
+++ src/pages/About.js
@@ -6,10 +6,16 @@ function About() {
   return (
     <div className="container">
         <p className="info">
-          Born and raised in Munich southern Gemany where i found my passion
-          in web develpment and UX design. Le Wagon teached me how to build
-          web applications from scretch and how to work in tech teams with
-          people from various backgrounds.
+          Born and raised in Munich southern Gemany and now living in Bern
+          Switzerland. Found my passion in coding as specially, in web
+          development and UX design after finishing a coding bootcamp. Since
+          then i spend my time to gaining proviciency in all intricacies of web
+          development in order to better my self as a developer ans problem solver.
+          I'm able to create clean and functional websites as well as professional
+          brandings. I'm confident in my frontend, backend and design abilities
+          to the point. I'm able to confidently deliver products to my clients
+          knowing that they are of high quality. I enjoy helping brnads establish
+          their digital presence and developing plesant user experiences.
         </p>
     </div>
   );
