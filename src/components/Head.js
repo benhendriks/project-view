fc8222daebcbb6a147842ef58c169cd4afaebe06
Benjamin Steidl <localartpool@gmail.com>
2020-09-01 19:50:05 +0200

    Worked on page title, links and meat tags

--- src/components/Head.js
+++ src/components/Head.js
@@ -7,17 +7,9 @@ const Head = () => {
     <Helmet>
       <title>Benjamin Steidl</title>
       <meta charset="utf-8" />
-      <meta name="description" content="Portfolio Benjamin Seidl" />
-      <link
-        rel="shortcut icon"
-        href="https://res.cloudinary.com/daf8fgekl/image/upload/v1598282248/logo_fyqsyy.svg"
-      />
+      <meta name="description" content="Portfolio Benjamin Steidl" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta name="theme-color" content="#000000" />
-      <link
-        rel="apple-touch-icon"
-        href="https://res.cloudinary.com/daf8fgekl/image/upload/v1598282248/logo_fyqsyy.svg"
-      />
     </Helmet>
   );
 };
