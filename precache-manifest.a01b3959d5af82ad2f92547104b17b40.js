d86e37161535b1a55ef1841313f5ffc3afbd414e
Benjamin Steidl <localartpool@gmail.com>
2020-09-01 19:54:34 +0200

    Updates

--- precache-manifest.a01b3959d5af82ad2f92547104b17b40.js
+++ precache-manifest.a01b3959d5af82ad2f92547104b17b40.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c6f141fd42ca0a469c4ae55686a8eed7",
+    "revision": "9cc205dd2a25f37c848aab3d42371e15",
     "url": "/index.html"
   },
   {
-    "revision": "db8954b8475b833f3cbb",
+    "revision": "086743af62260b17263d",
     "url": "/static/css/main.8d362a42.chunk.css"
   },
   {
-    "revision": "9fc3715d1a649f1466da",
-    "url": "/static/js/2.0159592b.chunk.js"
+    "revision": "3d0cc26b4c50e8a5e283",
+    "url": "/static/js/2.487e192e.chunk.js"
   },
   {
     "revision": "f5327a3e8204f21ff1b2e5af38bc59ba",
-    "url": "/static/js/2.0159592b.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.487e192e.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "db8954b8475b833f3cbb",
-    "url": "/static/js/main.3d0f261c.chunk.js"
+    "revision": "086743af62260b17263d",
+    "url": "/static/js/main.02bb5978.chunk.js"
   },
   {
     "revision": "65f8c86f4c8250bd2868",
